8acbef3d7d2bb9a8133bd2f6858037245751b6de
Dipen Vasoya <dipvasoya2310@gmail.com>
2023-11-11 11:27:49 +0530

    DB COn

--- yaarvi-demo/conn.php
+++ yaarvi-demo/conn.php
@@ -1,4 +1,4 @@
 <?php
-    $link=mysqli_connect("localhost","root","") or die(mysqli_error("not connect"));
-    mysqli_select_db($link,"yaarvi_db")or die(mysqli_error($link));
+    $link=mysqli_connect("localhost","u115505489_demoyaarvi","#DemoYaarvi2023") or die(mysqli_error("not connect"));
+    mysqli_select_db($link,"u115505489_demoyaarvi")or die(mysqli_error($link));
 ?>
